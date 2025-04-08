f35b4033baadf0f38485665876599fb29faf8d57
jimenezz22 <ljimenezbogantes@gmail.com>
2025-04-07 20:58:38 -0600

    fix: update import path for Button component in AnnouncementCard

--- client/src/components/ui/AnnouncementCard.tsx
+++ client/src/components/ui/AnnouncementCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Button } from "./Button";
 import { ArrowRight } from "lucide-react";
 
 export interface AnnouncementData {
